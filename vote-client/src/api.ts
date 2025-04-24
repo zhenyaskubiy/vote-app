a7dc1bb31d7cf6102b0cbc62f74e892ebea84a98
vdmytriuk <valentyn@trainual.com>
2025-04-24 12:16:57 +0300

    Set up initial Rails API with Polls and Votes functionality

--- vote-client/src/api.ts
+++ vote-client/src/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://192.168.1.121:3000/api/v1',
+  baseURL: 'http://127.0.0.1:3000/api/v1',
 });
--- vote-client/src/cable.ts
+++ vote-client/src/cable.ts
@@ -1,3 +1,3 @@
 import * as ActionCable from '@rails/actioncable';
 
-export const cable = ActionCable.createConsumer('ws://192.168.1.121:3000/cable');
+export const cable = ActionCable.createConsumer('ws://127.0.0.1:3000/cable');
